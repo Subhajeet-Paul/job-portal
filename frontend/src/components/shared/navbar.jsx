92e7052825bb13b2c83c71899f233bb13d388fe6
Subhajeet-Paul <subhajeetpaul28@gmail.com>
2025-09-07 21:30:42 +0530

    working with frontend

--- frontend/src/components/shared/navbar.jsx
+++ frontend/src/components/shared/navbar.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
 
+// import {Link} from 'react-router-dom'
 const navbar = () => {
   return (
     <div className="bg-white">
-        <h1 className='text-2xl font-bold'>Job <span className='tex[#F83002]'>Portal</span></h1>
-    </div>
-  )
-}
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job <span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+        <div className="flex items-center gp-2">
+          <ul className="flex items-center font-medium gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
 
-export default navbar
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent>Place content for the popover here.</PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
