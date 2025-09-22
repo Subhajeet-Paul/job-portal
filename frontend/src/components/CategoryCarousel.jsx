072c10f5eeed675750dca879a7892efff74853d8
Subhajeet-Paul <subhajeetpaul28@gmail.com>
2025-09-23 00:47:03 +0530

    working with UI

---
+++ frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const category =[
+    "Frontend Developer",
+    "Backend Developer",
+    "UI/UX Designer",
+    "Fullstack Developer",
+    "Mobile Developer",
+    "Data Scientist",
+]
+const CategoryCarousel = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CategoryCarousel
--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Navbar from "./shared/navbar.jsx"
+import HeroSection from './HeroSection.jsx'
+import CategoryCarousel from './CategoryCarousel.jsx'
 const home = () => {
   return (
     <>
     <Navbar/>
+    <HeroSection/>
+    <CategoryCarousel/>
+    {/* <LatestJobs/>
+    <Footer/> */}
     </>
   )
 }
